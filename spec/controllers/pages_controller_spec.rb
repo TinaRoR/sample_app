85020294fbf815a83b1cec4db9777a2b4c5f6930
Tina <gemalescuflorentina@live.com>
2011-08-07 01:44:09 +0300

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,5 +15,10 @@ describe PagesController do
       response.should be_success
     end
   end
-
+describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
